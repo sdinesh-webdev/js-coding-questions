b1be8a76cea64d6175a4fcadd85c524e9fe29a52
S DINESH KUMAR <jhmuk7899@gmail.com>
2025-10-28 20:47:08 +0530

    Update Alternative_Element.js

--- ARRAY/Alternative_Element.js
+++ ARRAY/Alternative_Element.js
@@ -1,1 +1,12 @@
+function getAlternates(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    res.push(arr[i]);
+  }
+  return res;
+}
+
+// Example usage:
+const arr = [10, 20, 30, 40, 50];
+console.log(getAlternates(arr)); // [10, 30, 50]
 
