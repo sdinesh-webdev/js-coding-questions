163d3c6e16f1c79c19352b1dc3fb2208b51e88d1
S DINESH KUMAR <jhmuk7899@gmail.com>
2025-10-29 22:04:25 +0530

    Update Lineat_Search.js

--- ARRAY/Lineat_Search.js
+++ ARRAY/Lineat_Search.js
@@ -1,1 +1,11 @@
 
+function linear(arr, x) {
+    for (let i=0; i< arr.length; i++) {
+        if(arr[i] === x) return i;
+    }
+    return `${x} not_found`;
+}
+
+const arr = [2,3,4,10,40];
+console.log(linear(arr, 10));
+console.log(linear(arr, 9));
